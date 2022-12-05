56e041a4a9abb1448d86e29045ce438b99b008ea
eduardocrispin <115659411+eduardocrispin@users.noreply.github.com>
2022-12-05 14:01:24 -0500

    calc

---
+++ Assignments/Calculator/js/calc.js
@@ -0,0 +1,46 @@
+
+//Store Result in Global Variable
+const result = document.getElementById("answer");
+
+
+//Write a demo function
+
+//Everytime you declare a function, you must call it afterwards.
+
+function resultAssign() {
+    num = 3 + 5 + 500 + 87 + 2;
+    result.innerHTML = num;
+    //console.log ("Variable num represents the number:" + num);
+}
+
+//Call the function
+//resultAssign()
+
+//Function to add Two and Two
+function AddTwoPlusTwo() {
+    result.innerHTML = 2 + 2;
+}
+
+function AddTwoMinusTwo() {
+    result.innerHTML = 2 - 2;
+}
+
+function AddThreeTimesTwo() {
+    result.innerHTML = 3 * 2;
+}
+
+function AddSixDvidedByTwo() {
+    result.innerHTML = 6 / 2;
+}
+
+function AddFiveDivdedByFive() {
+    result.innerHTML = 15 / 5;
+}
+
+function AddThreeTimesFive() {
+    result.innerHTML = 3 * 5;
+}
+
+resi.style.backgroundColor = "lightpink" 
+
+
